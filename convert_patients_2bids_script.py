da1e29efe21821718fdf40337584cf6282de06e7
ColinVDB <colin.vandenbulcke@uclouvain.be>
2021-11-09 11:05:55 +0100

    convert_patients_2bids_script

---
+++ convert_patients_2bids_script.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 10:57:14 2021
+
+@author: ColinVDB
+
+Script to transform a list of raw dicoms into a BIDS database 
+"""
+
+from dicom2bids import BIDSHandler
+
+dicom2niix_path = r"C:\Users\Cristina\Downloads\dcm2niix.exe"
+
+list_dicoms = ['',
+               ''
+               ]
+
+bidshandler = BIDSHandler(root_dir=r'', dicom2niix_path=dicom2niix_path)
+
+for dicom in list_dicoms:
+    
+    DICOM_FOLDER = dicom
+    PATIENT_ID = None
+    SESSION = None
+    
+    pat_id, session, dicom_series = bidshandler.convert_dicoms_to_bids(dicomfolder = DICOM_FOLDER, 
+                                                                        pat_id      = PATIENT_ID,
+                                                                        session     = SESSION, 
+                                                                        return_dicom_series=True)
+    print(f"[INFO] done for patient {pat_id}")
+print("[INFO] All done.")
